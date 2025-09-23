3f6e1ea76347c593757631756c5a8e7dc0b26c3a
Pradeep yadav <126454840+PradeepCodes38@users.noreply.github.com>
2025-09-23 16:01:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.use((err, req, res, next) => {
 
 // 404 handler
 app.use("*", (req, res) => {
-  res.status(404).json({ success: false, message: "Route not found" });
+  res.status(404).json({ success: false, message: "Route not" });
 });
 
 // Start Server
